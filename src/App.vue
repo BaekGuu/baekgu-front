2b6030964d504676864b576b3f567f963005eb22
ramrami-B <hrb.harper@gmail.com>
2024-05-03 10:54:45 +0900

    Init: project setting

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">메인화면</RouterLink>
+      <RouterLink to="/board">게시판</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/BoardView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>게시판</div>
+</template>
+
+<style scoped></style>
