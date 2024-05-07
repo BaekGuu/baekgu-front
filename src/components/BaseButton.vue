0405febf16d7aaa9680174ccb791b95de1bc6cc5
ramrami-B <hrb.harper@gmail.com>
2024-05-07 16:41:52 +0900

    Feat: 로그인, 회원가입 모달 생성

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({ text: String, isActive: Boolean });
+</script>
+
+<template>
+  <button :class="isActive ? 'bg-point' : 'disabled'">{{ text }}</button>
+</template>
+
+<style scoped>
+button {
+  padding: 0.8rem 1rem;
+  outline: none;
+  border: none;
+  border-radius: 15px;
+  font-size: 1rem;
+  color: white;
+}
+.disabled {
+  background-color: #d9d9d9;
+}
+</style>
---
+++ src/components/modal/ModalSignup.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BaseButton from "../BaseButton.vue";
+</script>
+
+<template>
+  <form action="">
+    <div>
+      <input type="text" placeholder="닉네임" />
+      <BaseButton text="중복확인" :isActive="true" />
+    </div>
+    <div>
+      <input type="text" placeholder="아이디" />
+      <BaseButton text="중복확인" :isActive="true"></BaseButton>
+    </div>
+    <input type="text" placeholder="비밀번호" /><input type="text" placeholder="비밀번호 확인" />
+    <span></span> <input type="text" placeholder="이메일" />
+    <BaseButton text="회원가입" :isActive="false" />
+  </form>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  padding: 4rem;
+  gap: 1rem;
+}
+
+form div {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+}
+</style>
