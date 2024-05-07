b6c1c4f33f6ae8412c5d976937fc9f5fcd01be53
ramrami-B <hrb.harper@gmail.com>
2024-05-07 12:10:21 +0900

    Feat: SearchView 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import MainView from "@/views/MainView.vue";
 import BoardView from "@/views/BoardView.vue";
+import MainView from "@/views/MainView.vue";
+import SearchView from "@/views/SearchView.vue";
 
 const router = createRouter({
+  linkActiveClass: "active",
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
       name: "main",
       component: MainView,
+    },
+    {
+      path: "/search",
+      name: "search",
+      component: SearchView,
     },
     {
       path: "/board",
