31d7a41bdb7e16e27fe5a8f80a884ded9c187ef4
ramrami-B <hrb.harper@gmail.com>
2024-05-08 10:33:36 +0900

    Feat: 소셜로그인 아이콘 추가

--- src/components/modal/ModalLogin.vue
+++ src/components/modal/ModalLogin.vue
@@ -7,6 +7,11 @@ import BaseButton from "../BaseButton.vue";
     <input type="text" placeholder="아이디" />
     <input type="password" placeholder="비밀번호" />
     <BaseButton text="로그인" :is-active="true" />
+    <div class="social">
+      <img src="../../assets/img/kakao_login.svg" alt="카카오로그인" />
+      <img src="../../assets/img/naver_login.svg" alt="네이버로그인" />
+      <img src="../../assets/img/google_login.svg" alt="구글로그인" />
+    </div>
   </form>
 </template>
 
@@ -16,5 +21,18 @@ form {
   flex-direction: column;
   padding: 4rem;
   gap: 1rem;
+}
+
+.social {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.social img {
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
 }
 </style>
