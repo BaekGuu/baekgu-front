e956ef57a582246f032e451ab9c3852fe0f5efae
ramrami-B <hrb.harper@gmail.com>
2024-05-08 16:58:18 +0900

    Refactor: http 상태값 정의

---
+++ src/constant/status.js
@@ -0,0 +1,14 @@
+// SUCCESS
+export const OK = 200;
+export const CREATED = 201;
+export const ACCEPTED = 202;
+export const NOCONTENT = 204;
+
+// ERROR
+export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
+export const METHOD_NOT_ALLOWED = 405;
+export const CONFLICT = 409;
+export const TOO_MANY_REQUESTS = 429;
